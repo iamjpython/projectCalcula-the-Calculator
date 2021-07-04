76d0a8b223893a7d5d7141df5d2f4d2f1a81e8e2
GwyneEscantilla <86350728+GwyneEscantilla@users.noreply.github.com>
2021-07-04 18:09:22 +0800

    Update ui.py
    Buttons under one class.

--- ui.py
+++ ui.py
@@ -1,32 +1,117 @@
-from tkinter import *
-root = Tk()
-root.title("Calculator")
+import tkinter as tk
+from tkinter.constants import E, END, SUNKEN, W
+class Calculator:
 
-entry = Entry(root, width=45, borderwidth=5, font=60)
-entry.grid(row=0, column=0,columnspan=5, padx=10, pady=30)
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.root.title("projectCalcula")
+        self.root.resizable(0,0)
+        self.e = tk.Entry(self.root, width=50, borderwidth=5, font=60, justify="right")#borderwidth design, font height ng input border
+        self.e.grid(row=0, column=0,columnspan=5, padx=10, pady=50) #pady gap between input and button
+        self.label = tk.Label(self.root, text = "projectCalcula", bd=1, relief=SUNKEN, font="italic").grid(row=8, column=0, columnspan=7, sticky=W+E)
 
-b1 = Button(root, text="1", padx=40, pady=10, command=lambda:button_click(1)).grid(row=3,column=2)
-b2 = Button(root, text="2", padx=40, pady=10, command=lambda:button_click(2)).grid(row=3,column=1)
-b3 = Button(root, text="3", padx=40, pady=10, command=lambda:button_click(3)).grid(row=3,column=0)
+        self.bsin = tk.Button(self.root, text="sin", padx=20, pady=5,bg = "light gray")
+        self.bcos = tk.Button(self.root, text="cos", padx=20, pady=5,bg = "light gray")
+        self.btan = tk.Button(self.root, text="tan", padx=20, pady=5,bg = "light gray")
+        self.blog = tk.Button(self.root, text="log", padx=21, pady=5,bg = "light gray")
+        self.bleft = tk.Button(self.root, text="(", padx=28, pady=5,bg = "light gray")
+        self.bright = tk.Button(self.root, text=")", padx=28, pady=5,bg = "light gray")
+        self.bsqrt = tk.Button(self.root, text="√", padx=25, pady=5,bg = "light gray")
 
-b4 = Button(root, text="4", padx=40, pady=10, command=lambda:button_click(4)).grid(row=2,column=2)
-b5 = Button(root, text="5", padx=40, pady=10, command=lambda:button_click(5)).grid(row=2,column=1)
-b6 = Button(root, text="6", padx=40, pady=10, command=lambda:button_click(6)).grid(row=2,column=0)
+        self.basin = tk.Button(self.root, text="asin", padx=17, pady=5,bg = "light gray")
+        self.bacos = tk.Button(self.root, text="acos", padx=17, pady=5,bg = "light gray")
+        self.batan = tk.Button(self.root, text="atan", padx=17, pady=5,bg = "light gray")
+        self.bln = tk.Button(self.root, text=" ln", padx=23, pady=5,bg = "light gray")
+        self.bpi = tk.Button(self.root, text="π", padx=26, pady=5,bg = "light gray")
+        self.bfac = tk.Button(self.root, text="!", padx=27, pady=5,bg = "light gray")
+        self.bsqr = tk.Button(self.root, text="x²", padx=24, pady=5,bg = "light gray")
+        self.bcube = tk.Button(self.root, text="x³", padx=24, pady=5,bg = "light gray")
 
-b7 = Button(root, text="7", padx=40, pady=10, command=lambda:button_click(7)).grid(row=1,column=2)
-b8 = Button(root, text="8", padx=40, pady=10, command=lambda:button_click(8)).grid(row=1,column=1)
-b9 = Button(root, text="9", padx=40, pady=10, command=lambda:button_click(9)).grid(row=1,column=0)
+        self.bans = tk.Button(self.root, text="ANS", padx=18, pady=10,bg = "light gray")
+ 
+ 
+        self.bsin.bind("<Button-1>",self.sccalc)
+        self.bsin.grid(row=1,column=0,pady=5)
 
-b0 = Button(root, text="0", padx=91, pady=10, command=lambda:button_click(0)).grid(row=4,column=0, columnspan=2)
-bdeci = Button(root, text=".", padx=42, pady=10, command=lambda:button_click(0)).grid(row=4,column=2)
+        self.bcos.bind("<Button-1>",self.sccalc)
+        self.bcos.grid(row=1,column=1,pady=5)
 
-badd = Button(root, text="+ ", padx=42, pady=10, command=lambda:button_click("+")).grid(row=1,column=3)
-bminus = Button(root, text="-", padx=45, pady=10, command=lambda:button_click("-")).grid(row=2,column=3)
-btimes = Button(root, text="x", padx=45, pady=10, command=lambda:button_click("*")).grid(row=3,column=3)
-bdivide = Button(root, text="  % ", padx=38, pady=10, command=lambda:button_click("/")).grid(row=4,column=3)
+        self.btan.bind("<Button-1>",self.sccalc)
+        self.btan.grid(row=1,column=2,pady=5)
 
-bdel = Button(root, text=" DEL ", padx=31, pady=10, command=lambda:button_click("del")).grid(row=1,column=4)
-bclear = Button(root, text="AC", padx=36, pady=10, command=lambda:button_click("ac")).grid(row=2,column=4)
-bequal = Button(root, text="=", padx=40, pady=33, command=lambda:button_click("=")).grid(row=3,column=4, rowspan=2)
+        self.blog.bind("<Button-1>",self.sccalc)
+        self.blog.grid(row=1,column=3,pady=5)
 
-root.mainloop()
+        self.bln.bind("<Button-1>",self.sccalc)
+        self.bln.grid(row=1,column=4,pady=5)
+
+        self.basin.bind("<Button-1>",self.sccalc)
+        self.basin.grid(row=2,column=0,pady=5)
+
+        self.bacos.bind("<Button-1>",self.sccalc)
+        self.bacos.grid(row=2,column=1,pady=5)
+
+        self.batan.bind("<Button-1>",self.sccalc)
+        self.batan.grid(row=2,column=2,pady=5)
+
+        self.bpi.bind("<Button-1>",self.sccalc)
+        self.bpi.grid(row=2,column=3,pady=5)
+
+        self.bfac.bind("<Button-1>",self.sccalc)
+        self.bfac.grid(row=2,column=4,pady=5)
+
+        self.bcube.bind("<Button-1>",self.sccalc)
+        self.bcube.grid(row=3,column=0,pady=5)
+
+        self.bsqr.bind("<Button-1>",self.sccalc)
+        self.bsqr.grid(row=3,column=1,pady=5)
+
+        self.bsqrt.bind("<Button-1>",self.sccalc)
+        self.bsqrt.grid(row=3,column=2,pady=5)
+
+        self.bleft.bind("<Button-1>",self.sccalc)
+        self.bleft.grid(row=3,column=3,pady=5)
+
+        self.bright.bind("<Button-1>",self.sccalc)
+        self.bright.grid(row=3,column=4,pady=5)
+        
+        self.bans.bind("<Button-1>",self.sccalc)
+        self.bans.grid(row=7,column=3,pady=5)
+
+
+        self.b1 =tk.Button(self.root, text="1", padx=25, pady=10, command=lambda:self.button_click(1),bg = "light gray").grid(row=6,column=0,pady=5)
+        self.b2 =tk.Button(self.root, text="2", padx=25, pady=10, command=lambda:self.button_click(2),bg = "light gray").grid(row=6,column=1,pady=5)
+        self.b3 =tk.Button(self.root, text="3", padx=25, pady=10, command=lambda:self.button_click(3),bg = "light gray").grid(row=6,column=2,pady=5)
+
+        self.b4 =tk.Button(self.root, text="4", padx=25, pady=10, command=lambda:self.button_click(4),bg = "light gray").grid(row=5,column=0,pady=5)
+        self.b5 =tk.Button(self.root, text="5", padx=25, pady=10, command=lambda:self.button_click(5),bg = "light gray").grid(row=5,column=1,pady=5)
+        self.b6 =tk.Button(self.root, text="6", padx=25, pady=10, command=lambda:self.button_click(6),bg = "light gray").grid(row=5,column=2,pady=5)
+
+        self.b7 =tk.Button(self.root, text="7", padx=25, pady=10, command=lambda:self.button_click(7),bg = "light gray").grid(row=4,column=0,pady=5)
+        self.b8 =tk.Button(self.root, text="8", padx=25, pady=10, command=lambda:self.button_click(8),bg = "light gray").grid(row=4,column=1,pady=5)
+        self.b9 =tk.Button(self.root, text="9", padx=25, pady=10, command=lambda:self.button_click(9),bg = "light gray").grid(row=4,column=2,pady=5)
+
+        self.b0 =tk.Button(self.root, text="0", padx=72, pady=10, command=lambda:self.button_click(0),bg = "light gray").grid(row=7,column=0,columnspan=2, pady=5,)
+        self.bdeci =tk.Button(self.root, text=". ", padx=25, pady=10, command=lambda:self.button_click("."),bg = "light gray").grid(row=7,column=2,pady=5)
+
+        self.badd =tk.Button(self.root, text=" +", padx=24, pady=10, command=lambda:self.button_click("+"),bg = "light gray").grid(row=6,column=3,pady=5)
+        self.bminus =tk.Button(self.root, text="- ", padx=26, pady=10, command=lambda:self.button_click("-"),bg = "light gray").grid(row=6,column=4,pady=5)
+        self.btimes =tk.Button(self.root, text="x", padx=27, pady=10, command=lambda:self.button_click("*"),bg = "light gray").grid(row=5,column=3,pady=5)
+        self.bdivide =tk.Button(self.root, text=" ÷ ", padx=22, pady=10, command=lambda:self.button_click("/"),bg = "light gray").grid(row=5,column=4,pady=5)
+
+        self.bdel =tk.Button(self.root, text="DEL", padx=20, pady=10, command=self.delete, bg = "orange", fg= "black").grid(row=4,column=3,pady=5)
+        self.bclear =tk.Button(self.root, text="AC", padx=22, pady=10, command=self.ac,bg = "orange", fg= "black").grid(row=4,column=4,pady=5)
+        self.bequal =tk.Button(self.root, text="=", padx=26, pady=10, command=self.evaluate, bg = "yellow").grid(row=7,column=4,pady=5)
+    
+    def button_click(self,num):
+        dis = self.e.get()
+        self.e.delete(0,END)
+        self.e.insert(0,str(dis)+str(num)) 
+
+    def run(self):
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    calc =Calculator()
+    calc.run()
