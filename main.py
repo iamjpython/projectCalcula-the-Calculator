88251d12c2f6c3732875f65fb2ea2967cc70e063
John Paul Aguilar <85820110+iamjpython@users.noreply.github.com>
2021-07-09 01:36:44 +0800

    FINAL
    Final main.py with the integration of the UI and calculator code. With the requirements.

---
+++ main.py
@@ -0,0 +1,14 @@
+import math
+import operator
+from pyparsing import *
+import tkinter as tk
+from tkinter.constants import E, END, SUNKEN, W
+from tkinter import *
+import webbrowser
+import tkinter.messagebox as message
+
+import calculator
+import ui
+
+if __name__ == "__main__":
+	ui.main()
